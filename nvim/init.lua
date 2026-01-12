c6c73b7d6decbd6ae5913490152d89ecdbf652a4
Umbrella-loner <robinmogha@outlook.com>
2026-01-13 00:03:15 +0530

    Clean up comments in nvim/init.lua
    Removed unnecessary comment lines from init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,4 @@
-───────────────────────────────────────────────────────────
--- Plugins Manager: lazy.nvim
--- ─────────────────────────────────────────────────────────────────────────────
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
